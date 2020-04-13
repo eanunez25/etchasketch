40de0591273bb637f9f3a96f67c8dd31780dc7fb
Elvin Nunez <eanunez25@gmail.com>
2020-04-12 22:19:27 -0500

    Add script.js

--- script.js
+++ script.js
@@ -7,18 +7,6 @@ for (let i = 0; i < units ** 2; i++) {
     gridContainer.appendChild(div);
 }
 
-/*
-const units = prompt("Length of each side?")
-document.documentElement.style.setProperty('--units', units);
-const gridContainer = document.querySelector('gridContainer');
-for (let i = 0; i < units; i++) {
-    let div = document.createElement('div');
-    div.className = 'grid';
-    div.innerHTML = i;
-    gridContainer.appendChild(div);
-}
-*/
-
 gridContainer.addEventListener('mouseover', function(e) {
     let hoveredDiv = e.target;
     hoveredDiv.className = 'coloredDiv';
